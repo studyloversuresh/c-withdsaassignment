2812f0f4b1d7e7ed2c35f4d869f4eadd00ca12ed
Suresh Sharma <158706652+studyloversuresh@users.noreply.github.com>
2024-02-13 10:00:22 +0530

    Create Q4predictTheoutput.cpp

---
+++ bacisassignment/Q4predictTheoutput.cpp
@@ -0,0 +1,11 @@
+// Q4. What is the output of this program?
+#include<iostream>
+using namespace std;
+    int main() {
+    int a = 4;
+    int b = 5;
+    a++, b--;
+    cout << ++a << " " << b--;
+    //output should be a = 6 and b = 4;
+
+}
