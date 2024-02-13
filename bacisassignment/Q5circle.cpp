4bd4eca34746292cf0b9d528b2f919e4022888f6
Suresh Sharma <158706652+studyloversuresh@users.noreply.github.com>
2024-02-13 10:02:27 +0530

    Create Q5circle.cpp

---
+++ bacisassignment/Q5circle.cpp
@@ -0,0 +1,13 @@
+// Q5. Write a program to find the circumference
+// of a circle with radius 10 in C++.
+#include<iostream>
+using namespace std;
+int main()
+{
+    float radius = 10;
+    float py = 22/7;
+    float circumference = 2*py*radius;
+    cout<<"Radius of circle : "<<radius<<endl;
+    cout<<"circumference of circle : "<<circumference<<endl;
+    return 0;
+}
