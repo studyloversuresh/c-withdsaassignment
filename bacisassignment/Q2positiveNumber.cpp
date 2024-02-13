d72e8197db05c00df06dd7dcf0b210911600dca0
Suresh Sharma <158706652+studyloversuresh@users.noreply.github.com>
2024-02-13 09:58:41 +0530

    Update Q2positiveNumber.cpp

--- bacisassignment/Q2positiveNumber.cpp
+++ bacisassignment/Q2positiveNumber.cpp
@@ -1,1 +1,12 @@
-
+// Q2. Write a program to print 10 using 2 positive 
+// numbers less than 6 in C++ as output.
+#include<iostream>
+using namespace std;
+int main(){
+    //print 10 using 2 positive number less than 6
+    cout<<5+5<<endl;
+    //we can also use two int variable to print 10
+    int a = 5;
+    int b = 5;
+    cout<<(a+b)<<endl;
+}
